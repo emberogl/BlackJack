9e2a389cf9c862d1a5dce60f649c4c3339a1022e
emberogl <embernebulan@gmail.com>
2023-09-13 09:50:09 +0200

    not-finished-yet v1

--- Model/DeckHandler.cs
+++ Model/DeckHandler.cs
@@ -1,7 +1,8 @@
 ﻿namespace BlackJack.Model
 {
     /// <summary>
-    /// Responsible for dealing the initial set of cards.
+    /// Responsible for dealing the initial set of cards,
+    /// and removing cards from the deck.
     /// </summary>
     public struct DeckHandler
     {
@@ -15,7 +16,19 @@
             Card[] playerHand = { deck[0], deck[1] };
             Card[] dealerHand = { deck[2], deck[3] };
 
+            // Removing the cards from the deck because they were given to the players.
+            Remove(0, 4, ref deck);
+
             return (playerHand, dealerHand);
+        }
+        // Removes terms from array by range
+        public static void Remove(int min, int max, ref Card[] deck) 
+        {
+            Card[] newDeck = new Card[deck.Length - (max - min)];
+            Array.Copy(deck, 0, newDeck, 0, min);
+            Array.Copy(deck, max, newDeck, min, deck.Length - max);
+
+            deck = newDeck;
         }
     }
 }
